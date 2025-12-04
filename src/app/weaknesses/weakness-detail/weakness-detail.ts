69bcead56dd4123700b6f5f32e7ac12b96cab062
Thymen Janson <s1146329@student.hsleiden.nl>
2025-12-04 14:41:43 +0100

    Feature: added all attributes to the weakness detailpages

--- src/app/weaknesses/weakness-detail/weakness-detail.ts
+++ src/app/weaknesses/weakness-detail/weakness-detail.ts
@@ -1,11 +1,13 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { Weakness } from '../../models/weakness.model';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { WeaknessService } from '../../services/weakness.service';
+import { Mitigation } from '../../models/mitigation.model';
+import { MitigationService } from '../../services/mitigation.service';
 
 @Component({
   selector: 'app-weakness-detail',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './weakness-detail.html',
   styleUrl: './weakness-detail.scss'
 })
@@ -13,31 +15,58 @@ export class WeaknessDetail implements OnInit{
   
   public weakness: Weakness | null = null;
   public weaknessId?: string;
+  public mitigations: Mitigation[] = [];
   public loadingWeakness: boolean = true;
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private weaknessService: WeaknessService,
+    private mitigationService: MitigationService,
     private cdr: ChangeDetectorRef
   ) { }
 
   ngOnInit(): void {
-    const id = this.activatedRoute.snapshot.paramMap.get('weaknessId');
-    if (!id) return;
+    this.activatedRoute.paramMap.subscribe(params => {
+      const id = params.get('weaknessId');
+      if (!id) return;
 
-    this.weaknessId = id;
+      this.weaknessId = id;
+      this.loadWeakness(id);
+    })
+  }
+
+  loadWeakness(id: string) {
+    this.loadingWeakness = true;
+    this.mitigations = [];
 
     this.weaknessService.getWeaknessById(id)
-        .subscribe({next: (weakness) => {
-          this.weakness = weakness;
-          this.loadingWeakness = false;
-          this.cdr.markForCheck();
-        }, error: err => {
-          console.error("Error occurred: ", err);
-          this.weakness = null;
-          this.loadingWeakness = false;
-          this.cdr.markForCheck();
+      .subscribe({next: (weakness) => {
+        this.weakness = weakness;
+
+        if (weakness.mitigations.length >= 1) {
+          for (const mitigationId of weakness.mitigations) {
+            this.mitigationService.getMitigationById(mitigationId)
+              .subscribe({next: (mitigation) => {
+                this.mitigations.push(mitigation);
+                this.loadingWeakness = false;
+                this.cdr.markForCheck();
+              }, error: err => {
+                console.error("Error occurred: ", err)
+                this.loadingWeakness = false;
+                this.cdr.markForCheck();
+              }
+            })
+          }
         }
-      });
+
+        this.loadingWeakness = false;
+        this.cdr.markForCheck();
+      }, error: err => {
+        console.error("Error occurred: ", err);
+        this.weakness = null;
+        this.loadingWeakness = false;
+        this.cdr.markForCheck();
+      }
+    });
   }
 }
