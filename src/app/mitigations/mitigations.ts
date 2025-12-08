e848f88bd3dc197e1a4e4ee8ed84a20f7c124058
Thymen Janson <s1146329@student.hsleiden.nl>
2025-12-08 11:42:29 +0100

    Feature: added sort and filter functions to the mitigation collectionpage

--- src/app/mitigations/mitigations.ts
+++ src/app/mitigations/mitigations.ts
@@ -2,17 +2,24 @@ import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Mitigation } from '../models/mitigation.model';
 import { MitigationService } from '../services/mitigation.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-mitigations',
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './mitigations.html',
   styleUrl: './mitigations.scss'
 })
 export class Mitigations implements OnInit {
   
-  public mitigationList: Mitigation[] =[];
+  public allMitigations: Mitigation[] = [];
+  public mitigationList: Mitigation[] = [];
   public loadingMitigations: boolean = true;
+  public selectedSort = 'idAsc';
+  public selectedFilters: { [key: string]: any } = {
+    technique: '',
+    references: ''
+  }
 
   constructor(
     private mitigationService: MitigationService,
@@ -28,8 +35,10 @@ export class Mitigations implements OnInit {
 
     this.mitigationService.getAllMitigations()
       .subscribe({next: (mitigations) => {
-        this.mitigationList = Object.values(mitigations);
+        this.allMitigations = Object.values(mitigations);
+        this.mitigationList = [...this.allMitigations];
         this.loadingMitigations = false;
+        this.applyFiltersAndSort();
         this.cdr.markForCheck();
       }, error: err => {
         console.error("Error: ", err)
@@ -37,5 +46,48 @@ export class Mitigations implements OnInit {
         this.cdr.markForCheck();
       }
     });
+  }
+
+  private isPresent(value: any): boolean {
+    if (value == null || value == undefined) return false;
+    if (typeof value == 'string') return value.trim() !== '';
+    if (Array.isArray(value)) return value.length > 0;
+    return true;
+  }
+
+  applyFiltersAndSort() {
+    this.mitigationList = this.allMitigations.filter(mitigation => {
+      for (let key of ['technique', 'references']) {
+        const filterValue = this.selectedFilters[key] as keyof typeof this.selectedFilters;
+        if (key == 'technique') {
+          if (filterValue == 'linked' && !this.isPresent((mitigation as any)[key])) return false;
+          if (filterValue == 'not linked' && this.isPresent((mitigation as any)[key])) return false;
+        } else if (key == 'references') {
+          if (filterValue == 'present' && !this.isPresent((mitigation as any)[key])) return false;
+          if (filterValue == 'absent' && this.isPresent((mitigation as any)[key])) return false;
+        }
+     }
+
+      return true;
+    });
+
+    this.sortList();
+  }
+
+  sortList() {
+    switch (this.selectedSort) {
+      case 'idAsc':
+        this.mitigationList.sort((a, b) => a.id.localeCompare(b.id));
+        break;
+      case 'idDesc':
+        this.mitigationList.sort((a, b) => b.id.localeCompare(a.id));
+        break;
+      case 'nameAsc':
+        this.mitigationList.sort((a, b) => a.name.localeCompare(b.name));
+        break;
+      case 'nameDesc':
+        this.mitigationList.sort((a, b) => b.name.localeCompare(a.name));
+        break;
+    }
   }
 }
